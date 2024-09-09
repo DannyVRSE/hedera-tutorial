573bebf46d75dccd254a204e7fe1e7e913113f48
Daniel Mshindi <mshindiouma1024@gmail.com>
2024-09-09 11:32:38 +0300

    func. (create account)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const {
     Hbar,
     Client,
+    PrivateKey, AccountCreateTransaction, AccountBalanceQuery, TransferTransaction,
 } = require("@hashgraph/sdk");
 
+// Load the environment variables
 require("dotenv").config();
 
 async function environmentSetup() {
@@ -30,5 +32,30 @@ async function environmentSetup() {
     client.setDefaultMaxQueryPayment(new Hbar(50));
 
     console.log("Client setup complete.");
+
+    //new key pair
+    const newAccountPrivateKey = PrivateKey.generateED25519();
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    //Create a new account with 1,000 tinybar starting balance
+    const newAccount = await new AccountCreateTransaction()
+        .setKey(newAccountPublicKey)
+        .setInitialBalance(Hbar.fromTinybars(1000))
+        .execute(client);
+
+    // Get the new account ID
+    const getReceipt = await newAccount.getReceipt(client);
+    const newAccountId = getReceipt.accountId;
+
+    //Log the account ID
+    console.log("The new account ID is: " + newAccountId);
+
+    //Verify the account balance
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybar.");
+
 }
 environmentSetup();
